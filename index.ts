2e099a9f58162b6f0090904ad9979e5edc04b2b1
yourgithubusername <yourgithubemail>
2024-03-31 03:23:20 +0100

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+const answers: any = await inquirer.prompt([
+  { message: "Enter your weight in Kg:", type: "number", name: "weightinKg"},
+ { message: "Enter your height in Meter:", type: "number", name: "heightinMeter"},
+  {
+    message: "Enter your weight in Kg:",
+    type: "number",
+    name: "weightinKg",
+
+ },
+
+{
+    message: " Enter height in meter:",
+    type: " number",
+    name: "heightinMeter"
+
+ },
+]);
+let bmi = answers.weightinKg/(answers.heightinMeter * answers.heightinMeter);
+
+if (bmi < 18.5){
+ console.log("underweight");
+}else if (bmi >=18.5 && bmi < 25 ){
+    console.log("normalweight");
+}else if (bmi >= 25 && bmi < 30){
+    console.log("overweight");
+}else{
+console.log("please enter a valid value");
+}
+console.log(`your BMI is ${bmi}`);
+        
+    
+
+    
+
+
